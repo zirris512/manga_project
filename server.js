a8e62ef95cf0f450532b8b1e290a1ef145f8afca
Brent <mina512@live.com>
2020-04-22 19:16:33 -0500

    Add eslint rules

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
    app.use(express.static('manga-app/build'));
-};
+}
 
 if (process.env.NODE_ENV === 'production') {
    app.get('*', (_, res) => {
       res.sendFile(path.join(__dirname, 'manga_app/build/index.html'));
    });
-};
+}
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}!`));
