94950698761816f298d4bf3316e2d2fc2896f55f
Brent <mina512@live.com>
2020-04-25 22:07:08 -0500

    Add route to login page

--- manga-app/src/components/Nav/index.js
+++ manga-app/src/components/Nav/index.js
@@ -12,7 +12,7 @@ function Nav() {
    return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
          <div className="container">
-            <a className="navbar-brand">Manga Project</a>
+            <p className="navbar-brand">Manga Project</p>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                <span className="navbar-toggler-icon"></span>
             </button>
@@ -26,7 +26,7 @@ function Nav() {
                   <input className="form-control" type="search" placeholder="Search" style={{"maxWidth": "50%"}}></input>
                   <button className="btn btn-light ml-2 input-group-append" type="submit" style={{"height": "40px", "width": "50px"}}><BsSearch style={{"fontSize": "25px"}} /></button>
                </form>
-               <a id="login" type="button" className="nav-link btn btn-danger mt-2 pt-1"><BsFillPersonFill style={iconStyle} /></a>
+               <Link id="login" type="button" className="nav-link btn btn-danger mt-2 pt-1" to="/login"><BsFillPersonFill style={iconStyle} /></Link>
             </div>
          </div>
       </nav>
