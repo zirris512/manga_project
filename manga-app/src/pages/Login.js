07c396c56bf46f299a1b9ff44a2f1f83c31d939a
Brent <mina512@live.com>
2020-05-21 21:20:43 -0500

    Add states to login/register pages

--- manga-app/src/pages/Login.js
+++ manga-app/src/pages/Login.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
+   const [credentials, setCredentials] = useState({
+      email: '',
+      password: '',
+      remember: false
+   });
+
+   useEffect(() => {
+      if (localStorage.remember && localStorage.email !== '') {
+         setCredentials({...credentials, remember: localStorage.remember, email: localStorage.email})
+      }
+   }, []);
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const { email, remember} = credentials;
+
+      if (remember && email !== '') {
+         localStorage.email = email;
+         localStorage.remember = remember;
+      }
+      if (!remember && localStorage.remember && localStorage.email) {
+         localStorage.removeItem('email');
+         localStorage.removeItem('remember');
+      }
+   }
+
    return (
       <div className='container my-4'>
          <div className='row d-flex justify-content-center'>
             <div className='col-md-4 login-form'>
-               <form>
+               <form onSubmit={handleSubmit}>
                   <h3 className='text-center'>Sign In</h3>
 
                   <div className='form-group'>
                      <label>Email address</label>
-                     <input type='email' className='form-control' placeholder='Enter email' />
+                     <input type='email' className='form-control' placeholder='Enter email' value={credentials.email} onChange={e => setCredentials({...credentials, email: e.target.value})} />
                   </div>
 
                   <div className='form-group'>
                      <label>Password</label>
-                     <input type='password' className='form-control' placeholder='Enter password' />
+                     <input type='password' className='form-control' placeholder='Enter password' value={credentials.password} onChange={e => setCredentials({...credentials, password: e.target.value})} />
                   </div>
 
                   <div className='form-group'>
                      <div className='custom-control custom-checkbox'>
-                        <input type='checkbox' className='custom-control-input' id='customCheck1' />
+                        <input type='checkbox' className='custom-control-input' id='customCheck1' checked={credentials.remember} onChange={e => setCredentials({...credentials, remember: e.target.checked})}/>
                         <label className='custom-control-label' htmlFor='customCheck1'>Remember me</label>
                      </div>
                   </div>
 
                   <button type='submit' className='btn btn-success btn-block'>Submit</button>
                   <p className='forgot-password text-right my-3'>
-                     <a href='/register'>Register?</a>
+                     <Link to='/register'>Register?</Link>
                   </p>
                </form>
             </div>
--- manga-app/src/pages/Register.js
+++ manga-app/src/pages/Register.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Register() {
+   const [formInput, setFormInput] = useState({
+      name: '',
+      email: '',
+      password: '',
+      password2: ''
+   });
+
+   const handleSubmit = event => {
+      event.preventDefault();
+      if (formInput.password !== formInput.password2) {
+         return console.error('Passwords must match!')
+      }
+      console.log(formInput);
+   }
+
    return (
       <div className='container my-4'>
          <div className='row d-flex justify-content-center'>
             <div className='col-md-4 login-form'>
-               <form>
+               <form onSubmit={handleSubmit}>
                   <h3 className='text-center'>Register</h3>
 
                   <div className='form-group'>
                      <label>Name</label>
-                     <input type='text' className='form-control' placeholder='Enter name' />
+                     <input type='text' name='name' value={formInput.name} onChange={e => setFormInput({...formInput, name: e.target.value})} className='form-control' placeholder='Enter name' />
                   </div>
 
                   <div className='form-group'>
                      <label>Email address</label>
-                     <input type='email' className='form-control' placeholder='Enter email' />
+                     <input type='email' name='email' value={formInput.email} onChange={e => setFormInput({...formInput, email: e.target.value})} className='form-control' placeholder='Enter email' />
                   </div>
 
                   <div className='form-group'>
                      <label>Password</label>
-                     <input type='password' className='form-control' placeholder='Enter password' />
+                     <input type='password' name='password' value={formInput.password} onChange={e => setFormInput({...formInput, password: e.target.value})} className='form-control' placeholder='Enter password' />
                   </div>
 
                   <div className='form-group'>
                      <label>Confirm Password</label>
-                     <input type='password' className='form-control' placeholder='Re-enter password' />
+                     <input type='password' name='password2' value={formInput.password2} onChange={e => setFormInput({...formInput, password2: e.target.value})} className='form-control' placeholder='Re-enter password' />
                   </div>
 
                   <button type='submit' className='btn btn-success btn-block'>Submit</button>
                   <p className='forgot-password text-right my-3'>
-                     <a href='/login'>Login?</a>
+                     <Link to='/login'>Login?</Link>
                   </p>
                </form>
             </div>
