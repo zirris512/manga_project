18b8191783a27f0252f218a0a42fff3e83164dd1
Brent <mina512@live.com>
2020-05-24 13:13:24 -0500

    Add register validation && heroku mLab settings

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,17 +5,40 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/api/register', (req, res) => {
-   const { user, email, password } = req.body;
+   const { email, password, password2 } = req.body;
+   let errorCount = 0;
+   const regValidation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,20}$/;
+   const mailValidation = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-   bcrypt.hash(password, 10, (err, hash) => {
-      if (err) throw err;
+   if (!email || !password || !password2) {
+      errorCount += 1;
+   }
+   if (!email.match(mailValidation)) {
+      errorCount += 1;
+   }
+   if (password !== password2) {
+      errorCount += 1;
+   }
+   if (!password.match(regValidation)) {
+      errorCount += 1;
+   }
 
-      User.create({ user, email, password: hash }, (error) => {
-         if (error) throw error;
-      });
-   });
+   if (errorCount === 0) {
+      User.findOne({ email })
+         // eslint-disable-next-line consistent-return
+         .then((result) => {
+            if (result) {
+               return res.sendStatus(409);
+            }
+            bcrypt.hash(password, 10, (err, hash) => {
+               if (err) throw err;
 
-   res.sendStatus(200);
+               User.create({ email, password: hash }, (error) => {
+                  if (error) throw error;
+               });
+            });
+         });
+   }
 });
 
 module.exports = router;
