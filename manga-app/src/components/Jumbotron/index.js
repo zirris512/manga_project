852718bde924f9e7a856019760e6884f5ae3ae7a
Brent <mina512@live.com>
2020-07-29 09:33:03 -0500

    Add Readme content

--- manga-app/src/components/Jumbotron/index.js
+++ manga-app/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Jumbotron = () => {
    return (
       <div className="jumbotron jumbotron-fluid text-center">
-         <h1 className="header-title">Manga Project</h1>
+         <h1 className="header-title">Manga Hub</h1>
       </div>
    );
 };
